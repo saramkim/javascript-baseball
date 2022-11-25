1609b9c79ba56fd5d5a2bacb5983e6de4c0550be
saram <yujun1018@naver.com>
2022-11-25 18:49:41 +0900

    feat: 동작을 위한 기능 연결

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+const BaseballGame = require('./model/BaseballGame');
+const AnswerMaker = require('./utils/AnswerMaker');
+const ResultPhraseMaker = require('./utils/ResultPhraseMaker');
+const InputView = require('./view/InputView');
+const OutputView = require('./view/OutputView');
+
 class App {
-  play() {}
+  #game;
+
+  play() {
+    const answer = AnswerMaker.makeAnswer();
+    this.#game = new BaseballGame(answer);
+    OutputView.printStart();
+    InputView.readAnswer(this);
+  }
+
+  compare(input) {
+    const result = this.#game.calculate(input);
+    const resultPhrase = ResultPhraseMaker.makeResultPhrase(result);
+    OutputView.printResult(resultPhrase);
+
+    if (this.#game.isCorrect(input)) {
+      OutputView.printComplete();
+      InputView.readRestart(this);
+    } else InputView.readAnswer(this);
+  }
 }
 
 module.exports = App;
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -5,23 +5,16 @@ const InputView = {
   READ_ANSWER_PHRASE: '숫자를 입력해주세요 : ',
   READ_RESTART_PHRASE: '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.',
 
-  readAnswer() {
+  readAnswer(app) {
     Console.readLine(this.READ_ANSWER_PHRASE, (input) => {
-      try {
-        Validator.validateAnswer(input);
-      } catch (error) {
-        Console.print(error.message);
-      }
+      Validator.validateAnswer(input);
+      app.compare(input);
     });
   },
 
-  readRestart() {
+  readRestart(app) {
     Console.readLine(this.READ_RESTART_PHRASE, (input) => {
-        try {
-            Validator.validateRestart(input);
-          } catch (error) {
-            Console.print(error.message);
-          }
+      Validator.validateRestart(input);
     });
   },
 };
