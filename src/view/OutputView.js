8e02de8ecff64d7401c77bde89e58015ba89699f
saram <yujun1018@naver.com>
2022-11-25 17:23:29 +0900

    feat: 게임 종료 문구 출력 기능 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -2,6 +2,7 @@ const { Console } = require('@woowacourse/mission-utils');
 
 const OutputView = {
   START_PHRASE: '숫자 야구 게임을 시작합니다.',
+  COMPLETE_PHRASE: '3개의 숫자를 모두 맞히셨습니다! 게임 종료',
 
   printStart() {
     Console.print(this.START_PHRASE);
@@ -9,6 +10,10 @@ const OutputView = {
 
   printResult(resultPhrase) {
     Console.print(resultPhrase);
+  },
+
+  printComplete() {
+    Console.print(this.COMPLETE_PHRASE);
   },
 };
 
